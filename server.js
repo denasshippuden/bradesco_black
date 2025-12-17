95d885318000ebbe99b0a57cffd03c2b46fdb085
BatePapoQuente <chathot4419@gmail.com>
2025-12-17 14:35:31 -0300

    manual

--- server.js
+++ server.js
@@ -343,7 +343,8 @@ app.get("/", sendIndex);
 app.get("/index.html", sendIndex);
 app.get("/consulta", sendConsulta);
 app.get("/consulta.html", sendConsulta);
-app.get("/consulta/*", sendConsulta);
+app.get("/consulta", sendConsulta);
+app.get("/consulta.html", sendConsulta);
 
 app.post("/api/login", async (req, res) => {
   const { user, pass } = req.body || {};
